da1b3fcdf52928b47c2a228d247f993b9c6c6ca9
谴责 <2770894499@qq.com>
2024-07-09 13:28:55 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+// 导入Inter字体
+import { Inter } from 'next/font/google';
+export const inter = Inter({ weight: '400', subsets: ['latin'] })
+
+// 导入Lusitana字体
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] })
